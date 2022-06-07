6b3f76c74b1474ae11eac0cca20efff6b4ecfddb
“Cory <coryurton@gmail.com>
2022-06-06 21:05:36 -0500

    more starting concepts

---
+++ src/src/StudentTest.java
@@ -0,0 +1,21 @@
+public class StudentTest
+{
+    public static void main(String[] args) {
+        //1 create new student object
+        Student s = new Student();
+
+        //2 set student's state/variables
+        s.id = 1000;
+        s.name = "joan";
+        s.gender = "male";
+
+        //3 update profile with correct name
+        s.updateProfile("john");
+    }
+
+    boolean updateProfile(String newName) {
+        name = newName;
+        return true;
+    }
+
+}
