6eaa91bffe3cc1fcde94e1532976aa5f3b862c28
“Cory <coryurton@gmail.com>
2022-06-09 10:26:54 -0500

    exercise 1 with modulus

--- src/src/MethodExercises.java
+++ src/src/MethodExercises.java
@@ -4,6 +4,14 @@ public class MethodExercises {
 
         System.out.println(doAddition(4, 5));
 
+        System.out.println(doSubtraction(5, 5));
+
+        System.out.println(doDivision(10, 2));
+
+        System.out.println(doMultiplication(3, 3));
+
+        System.out.println(findRemainder(4, 2));
+
     }
 
     public static int doAddition(int x, int y) {
@@ -26,6 +34,12 @@ public class MethodExercises {
     public static int doDivision(int x, int y) {
 
         return x / y;
+
+    }
+
+    public static int findRemainder(int x, int y) {
+
+        return x % y;
 
     }
 }
