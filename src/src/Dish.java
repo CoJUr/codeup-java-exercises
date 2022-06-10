607fa9fa49618cdcd5bf640448e513de99e9a993
“Cory <coryurton@gmail.com>
2022-06-10 11:09:59 -0500

    synching up

---
+++ src/src/Dish.java
@@ -0,0 +1,11 @@
+public class Dish {
+
+    int costInCents;
+    String nameOfDish;
+    boolean wouldRecommend;
+
+    public static void printSummary() {
+        System.out.printf("");
+    }
+
+}
