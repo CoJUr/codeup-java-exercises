860f118efa901cd046fc657a607f5a8356c21f0a
“Cory <coryurton@gmail.com>
2022-06-10 11:49:14 -0500

    exercise complete

--- src/src/Dish.java
+++ src/src/Dish.java
@@ -1,11 +1,14 @@
 public class Dish {
 
-    int costInCents;
-    String nameOfDish;
-    boolean wouldRecommend;
+    public int costInCents;
+    public String nameOfDish;
+    public boolean wouldRecommend;
+    public String flavor;
 
-    public static void printSummary() {
-        System.out.printf("");
+
+
+    public void printSummary() {
+        System.out.printf("Cost: %d Name: %s Recommended: %b", this.costInCents, this.nameOfDish, this.wouldRecommend);
     }
 
 }
--- src/src/DishTest.java
+++ src/src/DishTest.java
@@ -1,4 +1,16 @@
 public class DishTest {
 
 
+    public static void main(String[] args) {
+        Dish dish = new Dish();
+        Dish dish1 = dish;
+
+        dish1.flavor = "catlike";
+        dish1.costInCents = 9000;
+        dish1.nameOfDish = "Cat meat platter";
+        dish1.wouldRecommend = true;
+
+        dish1.printSummary();
+
+    }
 }
