579646c7892911cc508be4e676dc5fd667b7cf6d
“Cory <coryurton@gmail.com>
2022-06-17 10:52:06 -0500

    failsafe

--- src/masterclass/ListsAndArrayList/GroceryList.java
+++ src/masterclass/ListsAndArrayList/GroceryList.java
@@ -4,5 +4,43 @@ import java.util.ArrayList;
 
 public class GroceryList {
 
-    private ArrayList
+
+    private ArrayList<String> groceryList = new ArrayList<>();
+
+    public void addGroceryItem(String item){
+
+        groceryList.add(item);
+    }
+
+    public void printGroceryList() {
+        System.out.println("You have " + groceryList.size() + " groceries on your list");
+        for (int i = 0; i < groceryList.size(); i++) {
+            System.out.println((i + 1 ) + ". " + groceryList.get(i));
+
+        }
+    }
+
+
+    //modify existing items
+    public void modifyGroceryItem(int position, String newItem) {
+        groceryList.set(position, newItem);
+        System.out.println("Grocery item " + (position + 1 ) + " has been modified"); //position + 1 b/c 0 indexed
+    }
+
+
+    //remove items
+    public void removeGroceryItem(int position) {
+        String theItem = groceryList.get(position);
+        groceryList.remove(position);
+    }
+
+    public String findItem(String searchItem) {
+//        boolean exists = groceryList.contains(searchItem); //verifies it exists only
+
+        int position = groceryList.indexOf(searchItem); //searches the list, finds the item & returns its indexed position
+        if (position >= 0 ){
+            return groceryList.get(position); // if found, return the contents of that entry
+        }
+        return null; //otherwise return null bc not found
+    }
 }
--- src/masterclass/ListsAndArrayList/Main.java
+++ src/masterclass/ListsAndArrayList/Main.java
@@ -1,6 +1,92 @@
 package masterclass.ListsAndArrayList;
 
+import java.util.Scanner;
+
 public class Main {
+
+    private static Scanner scanner = new Scanner(System.in);
+    private static GroceryList groceryList = new GroceryList();
+
+    public static void main(String[] args) {
+        boolean quit = false;
+        int choice = 0;
+        printInstructions();
+        while(!quit) { //continue until user decides to stop
+            System.out.println("Enter your choice: ");
+            choice = scanner.nextInt(); // they select from a list of options of numbers
+            scanner.nextLine(); // clear the ghost
+
+            switch (choice) {
+                case 0:
+                    printInstructions();
+                    break;
+                case 1:
+                    groceryList.printGroceryList();
+                    break;
+                case 2:
+                    addItem();
+                    break;
+                case 3:
+                    modifyItem();
+                    break;
+                case 4:
+                    removeItem();
+                    break;
+                case 5:
+                    searchForItem();
+                    break;
+                case 6:
+                    quit = true;
+                    break;
+            }
+        }
+    }
+
+    public static void printInstructions() {
+        System.out.println("\nPress ");
+        System.out.println("\t 0 - to print choice options.");
+        System.out.println("\t 1 - to print the list of groceries.");
+        System.out.println("\t 2 - to add an item to the list.");
+        System.out.println("\t 3 - to modify an item of the list.");
+        System.out.println("\t 4 - to REMOVE and item from the list.");
+        System.out.println("\t 5 - to search the list for an item.");
+        System.out.println("\t 6 - to quit.");
+
+    }
+
+    public static void addItem() {
+        System.out.print("Please enter the grocery item: ");
+        groceryList.addGroceryItem(scanner.nextLine()); //whatever they type, add as an item
+
+    }
+
+    public static void modifyItem() {
+        System.out.print("Enter item # to be modified: ");
+        int itemNo = scanner.nextInt();
+        scanner.nextLine(); //clear the ghost
+        System.out.println("Enter replacement item: ");
+        String newItem = scanner.nextLine();
+        groceryList.modifyGroceryItem(itemNo -1, newItem); //itemNo - 1 because 0 index
+    }
+
+    public static void removeItem() {
+        System.out.print("Enter item # to be REMOVED: ");
+        int itemNo = scanner.nextInt();
+        scanner.nextLine(); //clear the ghost
+        groceryList.removeGroceryItem(itemNo -1); // note: no -1 here (causes out of bounds exception)
+    }
+
+    public static void searchForItem() {
+        System.out.print("Enter the item you're looking for: ");
+        String searchItem = scanner.nextLine();
+        if (groceryList.findItem(searchItem) != null ) {
+            System.out.println("Found " + searchItem + " in the list");
+        } else {
+            System.out.println(searchItem + " is not found in the list");
+        }
+    }
+
+
 
 
 
